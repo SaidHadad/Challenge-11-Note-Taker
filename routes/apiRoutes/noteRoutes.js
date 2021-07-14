ec20b24697f382abbd7ed1db1353bf47338e11fc
SaidHadad <saiddavid.hadad@gmail.com>
2021-07-14 17:48:27 -0400

    Get, Post routes
    added get and post routes for api and html notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,21 +1,24 @@
 const router = require('express').Router();
+const { createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
-// api call response for the notes, an having results sent to browser in the form of an array of object
-router.get("/notes", function (err, res) {
-  try {
-    createNoteData = fs.readFileSync("db/db.json", "utf8");
-    console.log("Hello from the SERVER!");
-    createNoteData = JSON.parse(createNoteData);
-  } catch (err) {
-    console.log("\n error (catch err app.get):");
-    console.log(err);
-  }
-  res.json(createNoteData);
+// get route for the notes
+router.get('/notes', (req, res) => {
+  let results = notes;
+  res.json(results);
 });
 
-router.get("/notes", function (req, res) {
-  return res.sendFile(path.json(__dirname, "db/db.json"));
+// port route for the notes
+router.post("/notes", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 module.exports = router;
