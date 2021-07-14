e67050568ffaeb822b2be297b8e3615c84bf155b
SaidHadad <saiddavid.hadad@gmail.com>
2021-07-14 19:36:58 -0400

    Delete route
    Added delete route, reworked on some code, and tested everything on Insomnia

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { createNewNote, validateNote } = require('../../lib/notes');
+const { createNewNote, validateNote, randomId } = require('../../lib/notes');
 const { notes } = require('../../db/db');
+const fs = require("fs");
 
 // get route for the notes
 router.get('/notes', (req, res) => {
@@ -8,17 +9,31 @@ router.get('/notes', (req, res) => {
   res.json(results);
 });
 
-// port route for the notes
+// post route for the notes
 router.post("/notes", (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
+  req.body.id = randomId();
 
   if (!validateNote(req.body)) {
-    res.status(400).send('');
+    res.status(400).send('Wrong format');
   } else {
     const note = createNewNote(req.body, notes);
     res.json(note);
   }
+});
+
+// delete route for notes
+router.delete('/notes/:id', (req, res) => {
+    const deleteNote = notes.findIndex((note) => note.id === req.params.id);
+
+    //splice the note
+    notes.splice(deleteNote, 1);
+
+    //update array after note got delated
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2), function(err) {
+        if (err) throw err;
+    });
+    res.json(deleteNote);
 });
 
 module.exports = router;
